af5274b6f130d03e46f49f4c5473ea423574e4a9
Easy Park <mrsimplelife@naver.com>
2019-04-03 13:24:45 +0900

    hi3

--- src/App.js
+++ src/App.js
@@ -1,37 +1,70 @@
 import React from 'react';
-import { Layout, Menu, Breadcrumb } from 'antd';
-
-const { Header, Content, Footer } = Layout;
-
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import img from './다운로드.jpg';
+import img1 from './17493_36286_524.jpg';
 const App = () => (
-	<Layout style={{}}>
-		<Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
-			<div
-				className="logo"
-				style={{
-					width: '120px',
-					height: '31px',
-					background: 'rgba(255,255,255,.2)',
-					margin: '16px 24px 16px 0',
-					float: 'left'
-				}}
-			/>
-			<Menu theme="dark" mode="horizontal" defaultSelectedKeys={[ '3' ]} style={{ lineHeight: '64px' }}>
-				<Menu.Item key="1">홈</Menu.Item>
-				<Menu.Item key="2">검색</Menu.Item>
-				<Menu.Item key="3">내책</Menu.Item>
-			</Menu>
-		</Header>
-		<Content style={{ padding: '0 50px', marginTop: 64 }}>
-			{/* <Breadcrumb style={{ margin: '16px 0' }}>
-				<Breadcrumb.Item>홈</Breadcrumb.Item>
-				<Breadcrumb.Item>List</Breadcrumb.Item>
-				<Breadcrumb.Item>App</Breadcrumb.Item>
-			</Breadcrumb> */}
-			<div style={{ background: '#fff', padding: 24, minHeight: 380 }}>Content</div>
-		</Content>
-		<Footer style={{ textAlign: 'center' }}>심심하다</Footer>
-	</Layout>
+	<Router basename={process.env.PUBLIC_URL}>
+		<div>
+			<ul>
+				<li>
+					<Link to="/">홈</Link>
+				</li>
+				<li>
+					<Link to="/books">내책</Link>
+				</li>
+			</ul>
+
+			<hr />
+			<Switch>
+				<Route exact path="/" component={Home} />
+				<Route exact path="/books" component={Books} />
+				<Route exact path="/book/:bookId" component={Book} />
+			</Switch>
+		</div>
+	</Router>
 );
+
+const Home = () => (
+	<div>
+		<h2>홈</h2>
+	</div>
+);
+
+const Books = ({ match }) => {
+	return (
+		<div>
+			<h2>내책</h2>
+			<div
+				style={{
+					display: 'grid',
+					gridTemplateColumns: 'repeat(auto-fit, 219px)',
+					gridGap: '59px',
+					justifyContent: 'center'
+				}}
+			>
+				<div>
+					<img src={img} alt="" style={{ width: '100%', height: '390px' }} />
+					<Link to="/book/1">오늘도 무사히</Link>
+				</div>
+				<div>
+					<img src={img1} alt="" style={{ width: '100%', height: '390px' }} />
+					<Link to="/book/2">나와 싸워 이기라</Link>
+				</div>
+				<div />
+				<div />
+				<div />
+			</div>
+		</div>
+	);
+};
+
+const Book = ({ match }) => {
+	console.log('실행');
+	return (
+		<div>
+			<h3>{match.params.bookId}</h3>
+		</div>
+	);
+};
 
 export default App;
